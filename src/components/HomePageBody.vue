063e4ec45cfb8345303779e0e13b861d849d0238
DunphyAl <dunphyal@tcd.ie>
2023-04-11 14:33:39 +0100

    first commit

---
+++ src/components/HomePageBody.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ItemTile from './ItemTile.vue'
+</script>
+
+<template>
+    <div>
+        <ItemTile>    
+            <template #heading>Contact Details</template>
+            <p>Email: alexander.g.dunphy@gmail.com</p>
+            <p>Mobile: 0834741044</p>
+            <p>LinkedIn: https://www.linkedin.com/in/alex-dunphy/</p>
+            <p>Github: https://github.com/Dunphyal</p>
+        </ItemTile>
+        <ItemTile>    
+            <template #heading>Education</template>
+            <p>B.A.I. in Computer and Electronic Engineering</p>
+            <p>Trinity College Dublin 2018 </p>
+        </ItemTile>
+        <ItemTile>    
+            <template #heading>Education</template>
+            <p>B.A.I. in Computer and Electronic Engineering</p>
+            <p>Trinity College Dublin 2018 </p>
+        </ItemTile>
+    </div>
+</template>
+
---
+++ src/components/ItemTile.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    let additionalInfo = ref('')
+
+    function addInfo() {
+        additionalInfo.value ? additionalInfo.value = '' : additionalInfo.value = 'new string'
+    }
+
+</script>
+
+<template>
+  <div class="item">
+    <div class="details">
+        <div class="title">
+            <h3>
+                <slot name="heading"></slot>
+            </h3>
+            <div @click="addInfo">X</div>
+        </div>
+      <slot></slot>
+      <div>{{additionalInfo}}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .item {
+        margin-top: 2rem;
+        padding: 2rem;
+        background-color:darkslateblue;
+        color:goldenrod;
+        border-radius: 2rem;
+    }
+
+    .title {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    h3 {
+        font-size: 1.2rem;
+        font-weight: bold;
+        margin-bottom: 0.4rem;
+    }
+</style>
